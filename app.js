5361eb405eda9b6d64f953ce64efd0bbc33e25b9
eGzIT <norbert.swieconek@gmail.com>
2021-09-11 14:10:17 +0200

    done project

---
+++ app.js
@@ -0,0 +1,61 @@
+const slides = document.querySelectorAll('.slide');
+const dots = document.querySelectorAll('.dot');
+const nextBtn = document.querySelector('.nextBtn');
+const prevBtn = document.querySelector('.prevBtn');
+
+let counter = 0;
+
+// position slides
+slides.forEach(function (slide, index) {
+    slide.style.left = `${index * 100}%`;
+});
+
+// arrows
+nextBtn.addEventListener('click', function () {
+    counter++;
+    carousel();
+});
+prevBtn.addEventListener('click', function () {
+    counter--;
+    carousel();
+});
+
+// dots
+dots.forEach(function (dot, index) {
+    dot.addEventListener('click', function() {
+        counter = index;
+        translateSlide();
+    })
+});
+
+// working with slides
+function carousel () {
+    if (counter === slides.length) {
+        counter = 0;
+    }
+    if (counter < 0) {
+        counter = slides.length - 1;
+    }
+
+    slides.forEach(function (slide) {
+        slide.style.transform = `translateX(-${counter * 100}%)`;
+    });  
+};
+
+// moving slides
+function translateSlide () {
+    slides.forEach(function (slide) {
+        slide.style.transform = `translateX(-${counter * 100}%)`;      
+    }); 
+};
+
+// Auto Slider
+setInterval(autoPlaying, 5000);
+
+function autoPlaying () {
+    counter++;         
+    if (counter >= 3) {
+        counter = 0;
+    };  
+    translateSlide();
+};
